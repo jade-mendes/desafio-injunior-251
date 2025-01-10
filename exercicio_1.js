b648556e1905d6de531c6dfe8cf0e0c20f6d23f5
Jade <jdmsa3@gmail.com>
2025-01-10 13:21:58 -0300

    feat: exercício 1

--- exercicio_1.js
+++ exercicio_1.js
@@ -1,5 +1,25 @@
-let a,b,c;
+function calcDelta(a, b, c){
+    return b**2 - 4*a*c;
+}
 
-a = "teste";
+function calcRoots(a, b, c){
+    let delta = calcDelta(a, b, c);
+    if (delta < 0){
+        console.log("Essa equação não possui raízes reais");
+        return
+    } else if (delta == 0){
+        let x1 = (-b + Math.sqrt(delta)) / 2*a;
+        console.log(`x1 = x2 = ${x1}`);
+    } else {
+        let x1 = (-b + Math.sqrt(delta)) / 2*a;
+        let x2 = (-b - Math.sqrt(delta)) / 2*a;
+        console.log(`x1 = ${x1}, x2 = ${x2}`);
+    }
 
-console.log(typeof a);
+}
+
+let a = parseFloat(prompt("Digite o coeficiente a:"));
+let b = parseFloat(prompt("Digite o coeficiente b:"));
+let c = parseFloat(prompt("Digite o coeficiente c:"));
+
+calcRoots(a, b, c);
